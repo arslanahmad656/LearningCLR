107e2e3f96b80a51e972f61a6cbf0d584ad72ac3
Arslan Ahmad <ars.ahm6@gmail.com>
2019-07-09 11:46:00 +0500

    Test class added

---
+++ LearningCLR/Project2/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal sealed class Test
+    {
+        // Constructor
+        public Test() { }
+
+        // Finalizer
+        ~Test() { }
+
+        // Operator overload
+        public static Boolean operator ==(Test t1, Test t2)
+        {
+            return true;
+        }
+
+        public static Boolean operator !=(Test t1, Test t2)
+        {
+            return false;
+        }
+
+        // An operator overload
+        public static Test operator +(Test t1, Test t2) { return null; }
+
+        // A property
+        public String AProperty
+        {
+            get { return null; }
+            set { }
+        }
+        // An indexer
+        public String this[Int32 x]
+        {
+            get { return null; }
+            set { }
+        }
+
+        // An event
+        public event EventHandler AnEvent;
+    }
+}
