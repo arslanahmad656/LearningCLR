69b41e7cb8320d48a7bb9268be776c31a1d70db8
Arslan Ahmad <ars.ahm6@gmail.com>
2019-07-11 10:53:41 +0500

    Conflict test example added

---
+++ LearningCLR/NonProject/Examples/CompilingCode/ConflictTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonProject.Examples.CompilingCode
+{
+    public class ConflictTest
+    {
+        public static void Main()
+        {
+            var result = ConflictingAssemblies.ConflictingAssembly.Method();    // ambiguity
+        }
+    }
+}
---
+++ LearningCLR/NonProject/Examples/CompilingCode/SameTypeAssemblies/ConflictingAssembly1.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConflictingAssemblies
+//{
+//    public class ConflictingAssembly
+//    {
+//        public static string Method()
+//        {
+//            return "Method in conflicting assembly1";
+//        }
+//    }
+//}
