cb5566a94dc1541e033d444a80bf97b1277e6057
Arslan Ahmad <ars.ahm6@gmail.com>
2019-07-11 16:04:14 +0500

    Multifile assembly example added

---
+++ LearningCLR/NonProject/Examples/CreatingMultifileAssembly/Type1.cs
@@ -0,0 +1,20 @@
+﻿// this file is inteded to be a module. Compile it with /t:module
+
+namespace CreatingMultifileAssembly
+{
+    public class TypeInModule1
+    {
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+
+    internal class InternalClassInModule1
+    {
+        public int HelloInternal()
+        {
+            return 1;
+        }
+    }
+}
---
+++ LearningCLR/NonProject/Examples/CreatingMultifileAssembly/Type2.cs
@@ -0,0 +1,13 @@
+﻿// This file is intended to be a library containing the self module and another module specified in Type1.cs
+// Compile this by using /t:library and /addmodule:<other-module-name>
+
+namespace CreatingMultifileAssembly
+{
+    public class Type2
+    {
+        public string HelloIn2()
+        {
+            return "Hello";
+        }
+    }
+}
